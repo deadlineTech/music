c78217d1697957ef0e6d1452dba71d7b3285c44d
Team DeadlineTech <tosu80018@gmail.com>
2026-02-27 01:03:35 +0530

    Update __init__.py

--- DeadlineTech/__init__.py
+++ DeadlineTech/__init__.py
@@ -18,10 +18,6 @@ userbot = Userbot()
 
 from .platforms import *
 
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
 Spotify = SpotifyAPI()
-Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()

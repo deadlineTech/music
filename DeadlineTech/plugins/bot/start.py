aeaf04ca13b85ecfa08f1de0b2b54fd973a9c310
Team DeadlineTech <tosu80018@gmail.com>
2026-02-27 05:25:13 +0530

    Update start.py

--- DeadlineTech/plugins/bot/start.py
+++ DeadlineTech/plugins/bot/start.py
@@ -14,19 +14,16 @@ from DeadlineTech.plugins.sudo.sudoers import sudoers_list
 from DeadlineTech.utils.database import (
     add_served_chat,
     add_served_user,
-    blacklisted_chats,
     get_lang,
-    is_banned_user,
     is_on_off,
 )
 from DeadlineTech.utils.decorators.language import LanguageStart
 from DeadlineTech.utils.formatters import get_readable_time
 from DeadlineTech.utils.inline import help_pannel, private_panel, start_panel
-from config import BANNED_USERS
 from strings import get_string
 
 
-@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["start"]) & filters.private)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
@@ -98,7 +95,7 @@ async def start_pm(client, message: Message, _):
             )
 
 
-@app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["start"]) & filters.group)
 @LanguageStart
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
@@ -117,26 +114,12 @@ async def welcome(client, message: Message):
         try:
             language = await get_lang(message.chat.id)
             _ = get_string(language)
-            if await is_banned_user(member.id):
-                try:
-                    await message.chat.ban_member(member.id)
-                except:
-                    pass
+            
             if member.id == app.id:
                 if message.chat.type != ChatType.SUPERGROUP:
                     await message.reply_text(_["start_4"])
                     return await app.leave_chat(message.chat.id)
-                if message.chat.id in await blacklisted_chats():
-                    await message.reply_text(
-                        _["start_5"].format(
-                            app.mention,
-                            f"https://t.me/{app.username}?start=sudolist",
-                            config.SUPPORT_CHAT,
-                        ),
-                        disable_web_page_preview=True,
-                    )
-                    return await app.leave_chat(message.chat.id)
-
+                
                 out = start_panel(_)
                 await message.reply_photo(
                     photo=config.START_IMG_URL,

8e176d5eb1fb9c9b12e24fc2e1cc98f001fa4b34
Team DeadlineTech <tosu80018@gmail.com>
2026-02-27 04:50:21 +0530

    Update extras.py

--- DeadlineTech/utils/inline/extras.py
+++ DeadlineTech/utils/inline/extras.py
@@ -1,13 +1,13 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from pyrogram import enums
 from config import SUPPORT_CHAT
 
 
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT, style=enums.ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=enums.ButtonStyle.DANGER),
         ],
     ]
     return buttons
@@ -20,6 +20,7 @@ def close_markup(_):
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
+                    style=enums.ButtonStyle.DANGER,
                 ),
             ]
         ]

b543323a66925dee62677a03621efb250ec34a47
Team DeadlineTech <tosu80018@gmail.com>
2026-02-27 03:36:39 +0530

    Update play.py

--- DeadlineTech/utils/inline/play.py
+++ DeadlineTech/utils/inline/play.py
@@ -73,7 +73,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="➕", callback_data=f"pl_menu|{chat_id}"),
+            InlineKeyboardButton(text="➕", callback_data=f"pl_save|{chat_id}"),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]

b2732dd1f8e381594eecb38a1712fa8227aed928
Team DeadlineTech <tosu80018@gmail.com>
2026-02-27 17:06:59 +0530

    Update mongo.py

--- DeadlineTech/core/mongo.py
+++ DeadlineTech/core/mongo.py
@@ -1,27 +1,9 @@
-
-
 # ==========================================================
 # 🎧 Public Open-Source VC Player Music Bot (Cookies Based)
 # 🛠️ Maintained by Team DeadlineTech | Lead Developer: @Its_damiann
 # 🔓 Licensed for Public Use — All Rights Reserved © Team DeadlineTech
-#
-# This file is part of a publicly available and open-source Telegram music bot
-# developed by Team DeadlineTech. It offers high-quality streaming in Telegram voice
-# chats using YouTube as a source, supported by session-based assistant accounts and
-# YouTube cookie integration for improved access and performance.
-#
-# 💡 This source code is released for educational and community purposes. You're free
-# to study, modify, and deploy it under fair and respectful usage. However, any misuse,
-# removal of credits, or false ownership claims will be considered a violation of our
-# community standards and may lead to denial of support or blacklisting.
-#
-# 🔗 Looking for powerful performance with stable APIs? Get access to the official
-# premium API service: https://DeadlineTech.site
-#
 # ❤️ Openly built for the community, but proudly protected by the passion of its creators.
 # ==========================================================
-
-
 
 
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -30,11 +12,11 @@ from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-LOGGER(__name__).info("⏳ Establishing a secure link to your MongoDB database...")
+LOGGER(__name__).info("Connecting to database :)")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Yukki
-    LOGGER(__name__).info("✅ Successfully connected to MongoDB. All systems are ready!")
+    mongodb = _mongo_async_.deadline
+    LOGGER(__name__).info("MongoDB Started Successfully :)")
 except:
-    LOGGER(__name__).error("❌ MongoDB connection failed!")
+    LOGGER(__name__).error("MongoDB connection failed!")
     exit()

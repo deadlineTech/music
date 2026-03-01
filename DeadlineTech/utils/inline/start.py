4dffb600ce2c33b3a867d031641d462c5a5ce1b7
Team DeadlineTech <tosu80018@gmail.com>
2026-03-02 01:11:29 +0530

    Update start.py

--- DeadlineTech/utils/inline/start.py
+++ DeadlineTech/utils/inline/start.py
@@ -42,8 +42,7 @@ def private_panel(_):
             
         ],
         [
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL), 
-            InlineKeyboardButton(text=_["S_B_5"], url="https://github.com/DeadlineTech/music")
+            InlineKeyboardButton(text=_["S_B_5"], url="https://github.com/DeadlineTech/music", style=enums.ButtonStyle.DANGER)
         ]
     ]
     return buttons

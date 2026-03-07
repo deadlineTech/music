25036f71d1ab859c31ee0f499ce9192819abc47d
Team DeadlineTech <tosu80018@gmail.com>
2026-03-08 00:28:23 +0530

    :)

--- DeadlineTech/plugins/misc/seeker.py
+++ DeadlineTech/plugins/misc/seeker.py
@@ -8,19 +8,27 @@ from DeadlineTech.utils.database import get_active_chats, is_music_playing
 
 async def timer():
     while not await asyncio.sleep(1):
-        active_chats = await get_active_chats()
-        for chat_id in active_chats:
-            if not await is_music_playing(chat_id):
-                continue
-            playing = db.get(chat_id)
-            if not playing:
-                continue
-            duration = int(playing[0]["seconds"])
-            if duration == 0:
-                continue
-            if db[chat_id][0]["played"] >= duration:
-                continue
-            db[chat_id][0]["played"] += 1
+        try:
+            active_chats = await get_active_chats()
+            for chat_id in active_chats:
+                if not await is_music_playing(chat_id):
+                    continue
+                playing = db.get(chat_id)
+                if not playing:
+                    continue
+                duration = int(playing[0]["seconds"])
+                if duration == 0:
+                    continue
+                if db[chat_id][0]["played"] >= duration:
+                    continue
+                db[chat_id][0]["played"] += 1
+        except Exception:
+            # Prevent task from dying on errors
+            continue
 
 
-asyncio.create_task(timer())
+# Task will be started from __main__.py after event loop is ready
+# DO NOT create task at module level - it causes event loop issues
+def start_timer_task():
+    """Call this function to start the timer task after event loop is ready."""
+    asyncio.create_task(timer())
